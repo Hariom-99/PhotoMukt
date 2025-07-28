b27dd457f0ef48e74f3f6e880c9b0f948c4040b7
Hariom-99 <hariomkhalghat@gmail.com>
2025-07-29 02:55:32 +0530

    Initial commit

---
+++ remover_bg.py
@@ -0,0 +1,19 @@
+import sys
+from rembg import remove
+
+if len(sys.argv) < 3:
+    print("Usage: python remover_bg.py <input_image> <output_image>")
+    sys.exit(1)
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+with open(input_path, 'rb') as i:
+    input_data = i.read()
+
+output_data = remove(input_data)
+
+with open(output_path, 'wb') as o:
+    o.write(output_data)
+
+print(f" Background removed and saved as: {output_path}")
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const multer = require('multer');
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.post('/upload', upload.single('image'), (req, res) => {
+  const inputPath = req.file.path;
+  const outputFilename = req.file.filename + '_output.png';
+  const outputPath = path.join(__dirname, 'public', outputFilename);
+
+  const scriptPath = path.join(__dirname, 'remover_bg.py');
+  const pythonPath = 'C:/Users/Asus/AppData/Local/Programs/Python/Python311/python.exe';
+  const command = `"${pythonPath}" "${scriptPath}" "${inputPath}" "${outputPath}"`;
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('<h2 style="color:red;">Background removal failed.</h2>');
+    }
+    console.log(stdout);
+
+    res.redirect(`/result.html?file=${outputFilename}`);
+
+    setTimeout(() => {
+      try {
+        fs.unlinkSync(inputPath);
+        fs.unlinkSync(outputPath);
+      } catch {};
+    }, 2 * 60 * 1000);
+  });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
